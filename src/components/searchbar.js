bac2794db6b653099a26fa14d9be4cb2597a84df
Brandon Johnson <bmj1985@gmail.com>
2018-04-15 14:37:24 -0600

    progressing

---
+++ src/components/searchbar.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render () {
+    return <input onChange={e => console.log(e.target.value)} />
+  }
+}
+
+export default SearchBar
+;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/searchbar'
 
 const API_KEY = 'AIzaSyDYD1J8Kj3571SVu5i8fHi1vvz4ZXpniLg'
 
-const App = () => <div><p>Hi!</p></div>
+const App = () => {
+    return (
+    <div>
+        <SearchBar></SearchBar>
+    </div>
+    )
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'))
